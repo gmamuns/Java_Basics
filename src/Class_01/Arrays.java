156f1bdbdd248bd161887ab27aab5f7a255bb36b
DELL <DELL@DESKTOP-3H3L1SM>
2017-07-20 03:44:52 +0600

    CRUD implementation using JSP and Plain Java

---
+++ src/Class_01/Arrays.java
@@ -0,0 +1,18 @@
+
+package Class_01;
+
+/**
+ *
+ * @author Mamuns
+ */
+public class Arrays {
+    
+    public static void main(String[] args) {
+         
+    
+        int[] anArray = {100, 200, 300,400, 500, 600,700, 800, 900, 1000};
+        System.out.println(anArray[1]); // Print the 2nd data of the array
+        System.out.println(anArray.getClass());
+    }
+    
+}
