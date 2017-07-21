689424cd392038c4d883535c5928f908226b7dcc
DELL <DELL@DESKTOP-3H3L1SM>
2017-07-21 10:33:18 +0600

    CRUD implementation using JSP and Plain Java

---
+++ src/Class_01/Primitive_casting.java
@@ -0,0 +1,19 @@
+
+package Class_01;
+
+import jdk.nashorn.internal.parser.TokenType;
+
+/**
+ *
+ * @author Mamuns
+ */
+public class Primitive_casting {
+    public static void main(String[] args) {
+        int a=3,b=4;
+        int rootValue = (int) Math.sqrt(a*b); //Casting
+        System.out.println(rootValue);
+       
+    }
+
+   
+}
---
+++ src/Class_01/Static_and_dynamic_initialization.java
@@ -0,0 +1,14 @@
+
+package Class_01;
+
+/**
+ *
+ * @author Mamuns
+ */
+public class Static_and_dynamic_initialization {
+    public static void main(String[] args) {
+        int a=3,b=4;
+        double rootValue = Math.sqrt(a*b); 
+        System.out.println(rootValue);
+    }
+}
