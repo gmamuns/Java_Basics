a8cba34fe58753914863e37b6d1bb786b962f2e7
Md Mamun Sikder <gmamuns@gmail.com>
2018-01-22 13:45:01 +0600

    Dependency of Enumerations example (Box.java)

---
+++ src/java_basics/Apple.java
@@ -0,0 +1,11 @@
+//This is an Enumerator
+//Note : Ennumerations is the fastest way to traverse Collection 
+/*
+MD MAMUN SIKDER
+gmamuns@gmail.com
+Date: 01 December 2017
+*/
+public enum Apple {
+    Jonathan, GoldenDel, RedDel, Winesap, Cortland
+    
+}
